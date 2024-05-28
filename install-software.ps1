d8c30b58c3ebcd9deffe924bb8e466e667e8531e
Alana de O. Rodrigues <alana.oliveira.rodrigues@gmail.com>
2024-05-28 15:16:01 -0300

    Update install-software.ps1

--- install-software.ps1
+++ install-software.ps1
@@ -1,3 +1,5 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 # Definir uma função para verificar se o Chocolatey está instalado
 function Install-Chocolatey {
     if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
